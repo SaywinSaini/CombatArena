50c029bc42c1b04e1c9b8b9e4939c1a069e2651f
SaywinSaini <vg7tzypwvv@privaterelay.appleid.com>
2026-05-09 15:53:01 +0530

    Add CAGameplayAbility base class
    - Created CAGameplayAbility inheriting from UGameplayAbility
    - Added ECAAbilityActivationPolicy enum with OnInputTriggered and OnGranted
    - Added ActivationPolicy property with default OnInputTriggered
    - Added GetActivationPolicy getter for external access

---
+++ Source/CombatArena/Abilities/CAGameplayAbility.h
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "CAGameplayAbility.generated.h"
+
+UENUM(BlueprintType)
+enum class ECAAbilityActivationPolicy : uint8
+{
+	OnInputTriggered,
+	OnGranted
+};
+
+UCLASS()
+class COMBATARENA_API UCAGameplayAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+	
+	public:
+	
+	// Returns the activation policy for this ability
+	ECAAbilityActivationPolicy GetActivationPolicy() const
+	{
+		return ActivationPolicy;
+	}
+	
+	protected:
+	
+	// Defines when this ability activates
+	UPROPERTY(EditDefaultsOnly, Category = "Ability")
+	ECAAbilityActivationPolicy ActivationPolicy = ECAAbilityActivationPolicy::OnInputTriggered;
+};
