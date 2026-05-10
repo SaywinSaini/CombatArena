859cb71d824c8a4030b53d07e806c1540d5aeee1
SaywinSaini <vg7tzypwvv@privaterelay.appleid.com>
2026-05-10 10:45:15 +0530

    Add CAProjectileAbility for ranged attacks
    - Created CAProjectileAbility inheriting from CAGameplayAbility
    - Added TSubclassOf<AActor> ProjectileClass slot for designer assignment
    - Added SpawnOffset to position projectile in front of character
    - ActivateAbility spawns projectile at offset location with character rotation
    - Null checks on character and ProjectileClass before spawning

---
+++ Source/CombatArena/Abilities/CAProjectileAbility.cpp
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CAProjectileAbility.h"
+
+#include "GameFramework/Character.h"
+
+void UCAProjectileAbility::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+                                           const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+                                           const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+	
+	ACharacter* Character = Cast<ACharacter>(ActorInfo->AvatarActor.Get());
+	
+	if (!Character) return;
+	
+	FVector SpawnLocation = Character->GetActorLocation() + (Character->GetActorForwardVector() * SpawnOffset);
+	
+	FRotator SpawnRotation = Character->GetActorRotation();
+	
+	if (!ProjectileClass) return;
+	
+	GetWorld()->SpawnActor<AActor>(ProjectileClass, SpawnLocation, SpawnRotation);
+	
+	EndAbility(Handle,ActorInfo,ActivationInfo,true,false);
+}
---
+++ Source/CombatArena/Abilities/CAProjectileAbility.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CAGameplayAbility.h"
+#include "CAProjectileAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COMBATARENA_API UCAProjectileAbility : public UCAGameplayAbility
+{
+	GENERATED_BODY()
+	
+	public:
+	
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+	
+protected:
+	
+	//Projectile class to spawn
+	UPROPERTY(EditDefaultsOnly, Category = "Abilities")
+	TSubclassOf<AActor> ProjectileClass;
+	
+	//Spawn offset from characrter location
+	UPROPERTY(EditDefaultsOnly, Category = "Abilities")
+	float SpawnOffset = 100.f;
+};
