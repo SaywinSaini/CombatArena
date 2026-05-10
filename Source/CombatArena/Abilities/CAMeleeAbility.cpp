8a45f471675e5796a3d61e4cec98c0c8279cc37c
SaywinSaini <vg7tzypwvv@privaterelay.appleid.com>
2026-05-10 10:11:39 +0530

    Add CAMeleeAbility with animation montage activation
    - Created CAMeleeAbility inheriting from CAGameplayAbility
    - Added AttackMontage property for animation assignment in Blueprint
    - ActivateAbility plays montage on character mesh
    - EndAbility called after montage plays
    - Null check on character before playing montage

---
+++ Source/CombatArena/Abilities/CAMeleeAbility.cpp
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CAMeleeAbility.h"
+
+#include "GameFramework/Character.h"
+
+void UCAMeleeAbility::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+                                      const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+                                      const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+	
+	ACharacter* Character = Cast<ACharacter>(ActorInfo->AvatarActor.Get());
+	
+	if (!Character) return;
+	
+	Character->PlayAnimMontage(AttackMontage);
+	
+	EndAbility(Handle,ActorInfo,ActivationInfo,true,false);
+	
+}
---
+++ Source/CombatArena/Abilities/CAMeleeAbility.h
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CAGameplayAbility.h"
+#include "CAMeleeAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COMBATARENA_API UCAMeleeAbility : public UCAGameplayAbility
+{
+	GENERATED_BODY()
+	
+	protected:
+	UPROPERTY(EditDefaultsOnly, Category = "Ability")
+	TObjectPtr<UAnimMontage> AttackMontage;
+	
+public:
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+};
