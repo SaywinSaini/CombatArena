8e58fb63a3e0cd2c0452c53c7984bd060aebdd9b
SaywinSaini <vg7tzypwvv@privaterelay.appleid.com>
2026-05-10 11:07:39 +0530

    Add CABlockAbility for defensive blocking
    - Created CABlockAbility inheriting from CAGameplayAbility
    - Added BlockDamageReduction property defaulting to 0.5 (50% damage reduction)
    - ActivateAbility starts block without calling EndAbility
    - OnBlockReleased ends the ability when block button is released

---
+++ Source/CombatArena/Abilities/CABlockAbility.cpp
@@ -0,0 +1,18 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CABlockAbility.h"
+
+void UCABlockAbility::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+	const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+	
+}
+
+void UCABlockAbility::OnBlockReleased(const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo)
+{
+	EndAbility(Handle,ActorInfo,ActivationInfo,true,false);
+}
---
+++ Source/CombatArena/Abilities/CABlockAbility.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CAGameplayAbility.h"
+#include "CABlockAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COMBATARENA_API UCABlockAbility : public UCAGameplayAbility
+{
+	GENERATED_BODY()
+	
+	public:
+	
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+	
+	// Ends the block ability when the player releases the block button
+	void OnBlockReleased(const FGameplayAbilitySpecHandle Handle,const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo);
+	
+	protected:
+	//Damage reduction percentage while blocking
+	UPROPERTY(EditDefaultsOnly, Category = "Ability")
+	float BlockDamageReduction = 0.5f;
+};
+
